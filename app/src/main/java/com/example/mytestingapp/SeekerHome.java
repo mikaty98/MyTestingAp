5c6799d0037d5b82c7d91977be7ceacfb674865f
Zeyad ElHalawany <whatelseisthere.5@gmail.com>
2021-01-19 23:36:56 +0200

    seeker home

--- app/src/main/java/com/example/mytestingapp/SeekerHome.java
+++ app/src/main/java/com/example/mytestingapp/SeekerHome.java
@@ -3,12 +3,21 @@ package com.example.mytestingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SeekerHome extends AppCompatActivity {
+
+    private Button localRequest;
+    private Button overboardRequest;
+    private Button officialRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seeker_home);
+
+        localRequest = findViewById(R.id.localService);
+        overboardRequest = findViewById(R.id.overboardService);
+        officialRequest = findViewById(R.id.officialService);
     }
 }
