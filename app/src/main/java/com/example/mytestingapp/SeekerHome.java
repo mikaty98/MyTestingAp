5fb3a97ff634adad9a7886ec17df6f93e0431408
Zeyad ElHalawany <whatelseisthere.5@gmail.com>
2021-01-20 23:52:05 +0200

    seeker home

--- app/src/main/java/com/example/mytestingapp/SeekerHome.java
+++ app/src/main/java/com/example/mytestingapp/SeekerHome.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,6 +14,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -22,7 +24,9 @@ public class SeekerHome extends AppCompatActivity {
     private Button localRequest;
     private Button overboardRequest;
     private Button officialRequest;
-    private EditText one, two;
+    public String inputEmail, userPassword, userName, age, id, phoneNumber, gender;
+
+
 
 
 
@@ -34,22 +38,68 @@ public class SeekerHome extends AppCompatActivity {
         Intent intent = getIntent();
         String text = intent.getStringExtra(SLoginActivity.TEXT);
 
-
-
-
-
         localRequest = findViewById(R.id.localService);
         overboardRequest = findViewById(R.id.overboardService);
         officialRequest = findViewById(R.id.officialService);
-        one = findViewById(R.id.one);
-        two = findViewById(R.id.two);
-
-        one.setError(text);
 
 
 
+        localRequest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                startActivity(new Intent(getApplicationContext(), SeekerLocalRequest.class));
+            }
+        });
+
+       // overboardRequest.setOnClickListener(new View.OnClickListener(){
+         //   @Override
+           // public void onClick(View v){
+
+             //   startActivity(new Intent(getApplicationContext(), PRegisterActivity.class));
+            //}
+        //});
 
 
+       // officialRequest.setOnClickListener(new View.OnClickListener(){
+         //   @Override
+           // public void onClick(View v){
+
+             //   startActivity(new Intent(getApplicationContext(), PRegisterActivity.class));
+            //}
+        //});
+
+
+
+
+
+        String parts [] = text.split(".com");
+        inputEmail = parts[0];
+
+
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Seekers");
+        Query checkUser = ref.orderByChild("email").equalTo(text);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                 userName = snapshot.child(inputEmail).child("userName").getValue(String.class);
+                 userPassword = snapshot.child(inputEmail).child("password").getValue(String.class);
+                 age = snapshot.child(inputEmail).child("age").getValue(String.class);
+                 gender = snapshot.child(inputEmail).child("gender").getValue(String.class);
+                 id = snapshot.child(inputEmail).child("id").getValue(String.class);
+                 phoneNumber = snapshot.child(inputEmail).child("phoneNumber").getValue(String.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/mytestingapp/SeekerLocalRequest.java
@@ -0,0 +1,14 @@
+package com.example.mytestingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SeekerLocalRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seeker_local_request);
+    }
+}
