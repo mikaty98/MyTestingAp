07c69fb8f1ef43b9edb34468bcaee08323099f70
abdelrhman srour <abdelrhmansrour98@gmail.com>
2021-02-23 17:53:49 +0200

    listview

--- app/src/main/java/com/example/mytestingapp/SeekerLocalRequestWaitingList.java
+++ app/src/main/java/com/example/mytestingapp/SeekerLocalRequestWaitingList.java
@@ -1,14 +1,68 @@
 package com.example.mytestingapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class SeekerLocalRequestWaitingList extends AppCompatActivity {
 
+    ListView myListView;
+
+    ArrayList<String> myArrayList = new ArrayList<>();
+
+    DatabaseReference mRef;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seeker_local_request_waiting_list);
+
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(SeekerLocalRequestWaitingList.this,android.R.layout.simple_list_item_1,myArrayList);
+
+        myListView = (ListView) findViewById(R.id.listview1);
+        myListView.setAdapter(myArrayAdapter);
+
+        mRef = FirebaseDatabase.getInstance().getReference();
+
+        mRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                myArrayList.add(value);
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
